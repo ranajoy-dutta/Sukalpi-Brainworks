eb923e78d4641b1cf96de814acc1fbe46f674707
ranajoy-dutta <ranajoydutta7@gmail.com>
2018-03-18 19:31:24 +0530

    Added Database
    Database has been connected to store the values of respective motors per
    second.

---
+++ Task 2/static/myscripts.js
@@ -0,0 +1,128 @@
+motorcount = 1;
+motorname = "Motor";
+mid = "csm"+motorcount;
+var ar = [];
+function addRow() {
+    var table = document.getElementById("motortable");
+    var table1 = document.getElementById("motort");
+    var rowCount = table.rows.length;
+    var row = table.insertRow(rowCount);
+    var row1 = table1.insertRow(rowCount);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    var c1 = row1.insertCell(0);
+    var c2 = row1.insertCell(1);
+    var c3 = row1.insertCell(2);
+    var ms = document.createElement('div');
+  	ms.className = 'currentspeed';
+  	ms.id = mid;
+    console.log(mid);
+  	ms.innerHTML  = '0';
+  	cell4.appendChild(ms);
+    cell1.innerHTML = motorname+motorcount;
+    c1.innerHTML = motorname+motorcount;
+    var DivOn = document.createElement('button');
+    DivOn.className = 'on';
+    DivOn.id=mid+'on';
+    DivOn.name=mid+'on';
+    DivOn.innerHTML  = 'ON';
+    motorcount++;
+    mid = "csm"+motorcount;
+  	DivOn.setAttribute('onclick',"motoron("+(motorcount-1)+")"); 
+  	cell2.appendChild(DivOn);
+  	var DivOff = document.createElement('button');
+  	DivOff.className = 'off';
+  	DivOff.innerHTML  = 'OFF'; 
+  	DivOff.setAttribute('onclick',"motoroff("+(motorcount-1)+")");
+    cell3.appendChild(DivOff);
+  	var up = document.createElement('button');
+  	up.className = 'speedchange';
+  	up.innerHTML  = 'UP'
+    up.setAttribute('onclick',"speedup("+(motorcount-1)+")");
+  	var down = document.createElement('button');
+  	down.className = 'speedchange';
+  	down.innerHTML  = 'Down' 
+    down.setAttribute('onclick',"speeddown("+(motorcount-1)+")");
+    cell5.appendChild(up);
+    cell5.appendChild(down);
+    var h = document.createElement('div');
+    h.className = 'health';
+    h.style.backgroundColor = 'green';
+    h.id = "health"+(motorcount-1);
+    c2.appendChild(h);
+    var fi = document.createElement('button');
+    fi.innerHTML = 'Fix Issues';
+    fi.id = "f"+(motorcount-1);
+    fi.setAttribute('onclick',"fix("+(motorcount-1)+")");
+    c3.appendChild(fi);
+}
+function motoron(x){
+  var t = "csm"+x;
+  var n = "csm"+x+'on';
+  document.getElementById(n).name = "csm"+x+'onn';
+	document.getElementById(t).innerHTML="5"; 
+}
+function motoroff(x){
+  var t = "csm"+x;
+  var n = "csm"+x+'on';
+  document.getElementById(n).name = "csm"+x+'on';
+	document.getElementById(t).innerHTML="0"; 
+}
+function speedup(x){
+  var t = "csm"+x;
+  var n = "csm"+x+'on';
+  var h = "health"+x;
+  var s = document.getElementById(t).innerHTML;
+  var nn = document.getElementById(n).name;
+  if(s>=0 && n!==nn){
+    s++;
+    document.getElementById(t).innerHTML=s;
+  }
+  if(s>10){
+    document.getElementById(h).style.backgroundColor = "Red";
+  }
+}
+function speeddown(x){
+  var t = "csm"+x;
+  var n = "csm"+x+'on';
+  var h = "health"+x;
+  var s = document.getElementById(t).innerHTML;
+  var nn = document.getElementById(n).name;
+  if(s>0 && n!=nn){
+    s--;
+    document.getElementById(t).innerHTML=s;
+  }
+  if(s<=10){
+    document.getElementById(h).style.backgroundColor = "green";
+  }
+}
+function fix(x){
+  var t = "csm"+x;
+  var n = "csm"+x+'on';
+  var h = "health"+x;
+  var s = document.getElementById(t).innerHTML;
+  var nn = document.getElementById(n).name;
+  if(n!==nn){
+    document.getElementById(t).innerHTML="5"; 
+    document.getElementById(h).style.backgroundColor = "green";
+  }
+}
+
+
+function calc(){
+  setInterval(function(){   
+  for (var i = 1; i < motorcount; i++) {
+    var t = "csm"+i;
+    var s = document.getElementById(t).innerHTML;
+    ar.push(s);
+  }
+  document.getElementById("re").value = ar;
+  for (var i = 1; i < motorcount; i++) {
+    ar.pop();
+  }
+  console.log(ar);
+  },10);
+}
---
+++ Task 2/testapp.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify, render_template, request, jsonify
+import sqlite3 as sql
+app = Flask(__name__)
+con = sql.connect('Task3.db')                              #Connection to DB
+cur = con.cursor()
+
+@app.route('/')
+def index():
+    return render_template('task2.html')
+
+@app.route('/_background_process')
+def background_process():
+    lang = request.args.get('proglang', 0, type=str)        #Fetching from HTML PAGE
+    motor_list=[]
+    if lang!="":
+         motor_list = list(map(int,lang.split(',')))
+    if len(motor_list)<1:
+       try:
+           cur.execute("DROP TABLE store ")                    #Dropping previous data
+           cur.execute("Create Table store(csm1)")              #creating fresh table
+           con.commit()
+       except:
+           print("Table Already Empty")
+    elif len(motor_list)>0:
+       cur.execute('PRAGMA TABLE_INFO(%s)'%('store'))         #Extracting Table Info
+       names = [tup[1] for tup in cur.fetchall()]             #Fetching Column Details
+       nI = len(names)
+       if (nI<len(motor_list)):                               #Inserting new column.. Handled for creation of more than one motor at a time
+           diff=len(motor_list)-nI
+           for i in range(1,diff+1):
+               cur.execute("ALTER TABLE store ADD COLUMN %s TEXT;"%('csm'+str(nI+i)))
+               con.commit()
+       cur.execute('PRAGMA TABLE_INFO(%s)'%('store'))         #Again fetching the total number of columns and their names for Dynamic insertion of data
+       names = [tup[1] for tup in cur.fetchall()]
+       cur.execute("INSERT INTO store(%s) VALUES(%s)"%(str(names)[1:-1],str(motor_list)[1:-1]))
+       con.commit()
+    return jsonify(True)
+
+            
+if __name__ == '__main__':
+     app.run()
